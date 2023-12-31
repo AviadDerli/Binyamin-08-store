bb6324fd2a18207c7429d90d4598393a435ff0b9
matanelvatkin <matanelvatkin@gmail.com>
2023-12-31 23:15:21 +0200

    location

--- src/ItemList.jsx
+++ src/ItemList.jsx
@@ -1,24 +1,29 @@
-import { useEffect, useState } from "react"
-import Item from "./Item"
+import { useEffect, useState } from "react";
+import Item from "./Item";
 
-export default function ItemList({category}) {
+export default function ItemList({ category }) {
+  const [items, setItems] = useState([]);
 
-    const [items,setItems] = useState([])
-
-    useEffect(()=>{
-        // /categories/alcohol ['','categories','category']
-        fetch('https://jbh-mockserver.onrender.com/categories/'+category.split('/')[2])
-        .then(j => j.json() )
-        .then(data=>setItems(data))
-    },[])
-
-
-    return (
-        <div>
-            <div id="itemList">
-                {items
-                    .map(product =><Item onClick={()=>location.href='/items/'+product.id}key={product.id} item={product} />)}
-            </div>
-        </div>
+  useEffect(() => {
+    // /categories/alcohol ['','categories','category']
+    fetch(
+      "https://jbh-mockserver.onrender.com/categories/" + category.split("/")[2]
     )
+      .then((j) => j.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  return (
+    <div>
+      <div id="itemList">
+        {items.map((product) => (
+          <Item
+            onClick={() => (location.href = "/items/" + product.id)}
+            key={product.id}
+            item={product}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
